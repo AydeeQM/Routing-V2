ab3840e32c0ca105500bfac16c87035f31717d8e
haidy <haidy>
2017-10-09 16:44:46 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -21,12 +21,22 @@ const Home = () => (
   </div>
 )
 
-const About = () => (
+const About = () => {
+      return(
   <div className="main-content">
     <h2 className="title">About</h2>
     <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
   </div>
-)
+    <div className="main-content">
+    <h2 className="title">About</h2>
+    <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+  </div>
+    <div className="main-content">
+    <h2 className="title">About</h2>
+    <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
+      </div>
+      ),
+}
  
 const Teachers = (props) => {
   let teachersList = [
